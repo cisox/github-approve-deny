b58dd855900d729450f7f77bfca7a8207c86085b
Brandon Peters <brandon.peters@gmail.com>
2013-06-26 20:16:17 -0400

    Added more error handling. Added strict string checking for Approve and Deny. Added support for comments with approve/deny message. Fixed button widths for 'Close' button width change.

--- github-approve-deny.user.js
+++ github-approve-deny.user.js
@@ -7,32 +7,41 @@
 
 (function() {
     var comments = document.getElementsByClassName('js-comment-body');
+
     for (var idx = 0; idx < comments.length; idx++) {
+        if (!comments[idx].children || comments[idx].children.length == 0) {
+            continue;
+        }
+
         var paragraph = comments[idx].children[0];
         if (!paragraph) {
             continue;
         }
 
-        if (paragraph.innerHTML.indexOf('+1') != -1
-            || paragraph.innerHTML.indexOf('Approve') != -1) {
-            paragraph.innerHTML = '';
+        if (paragraph.innerHTML.indexOf('+1 Approve') != -1) {
+            paragraph.innerHTML = paragraph.innerHTML.replace('+1 Approve', '');
             
             var approval = document.createElement('div');
             approval.className = 'state-indicator open js-comment-approved';
             approval.appendChild(document.createTextNode('Approved'));
 
-            paragraph.appendChild(approval);
-        } else if (paragraph.innerHTML.indexOf('-1') != -1
-            || paragraph.innerHTML.indexOf('Disapprove') != -1
-            || paragraph.innerHTML.indexOf('Deny') != -1
-            || paragraph.innerHTML.indexOf('Denied') != -1) {
-            paragraph.innerHTML = '';
+            if (paragraph.firstChild) {
+                paragraph.insertBefore(approval, paragraph.firstChild);
+            } else {
+                paragraph.appendChild(approval);
+            }
+        } else if (paragraph.innerHTML.indexOf('-1 Deny') != -1) {
+            paragraph.innerHTML = paragraph.innerHTML.replace('-1 Deny', '');
             
             var denial = document.createElement('div');
             denial.className = 'state-indicator closed js-comment-denied';
             denial.appendChild(document.createTextNode('Denied'));
 
-            paragraph.appendChild(denial);
+            if (paragraph.firstChild) {
+                paragraph.insertBefore(denial, paragraph.firstChild);
+            } else {
+                paragraph.appendChild(denial);
+            }
         }
     }
 
@@ -59,9 +68,11 @@
 
     var approveButton = document.createElement('button');
     approveButton.className = 'button primary js-approve-button';
+    approveButton.style.width = '70px';
+    approveButton.style.padding = '7px';
     approveButton.tabIndex = 4;
     approveButton.onclick = function(e) {
-        commentBody.value = '+1 Approve';
+        commentBody.value = '+1 Approve ' + commentBody.value;
         newCommentForm.submit();
 
         return false;
@@ -71,9 +82,11 @@
 
     var denyButton = document.createElement('button');
     denyButton.className = 'button danger js-deny-button';
+    denyButton.style.width = '55px';
+    denyButton.style.padding = '7px';
     denyButton.tabIndex = 5;
     denyButton.onclick = function(e) {
-        commentBody.value = '-1 Deny';
+        commentBody.value = '-1 Deny ' + commentBody.value;
         newCommentForm.submit();
 
         return false;
